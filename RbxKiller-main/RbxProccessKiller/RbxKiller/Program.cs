cd2a5531868c43c30ec556c367816589d567eecb
ILove-wheightlifting <130897982+ILove-wheightlifting@users.noreply.github.com>
2023-05-12 21:51:55 -0400

    Add files via upload

---
+++ RbxKiller-main/RbxProccessKiller/RbxKiller/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+
+Console.WriteLine("Welcome to RbxKiller! This program is mean't to kill ROBLOX and ROBLOX Studio, when it crashes and you can't close it!");
+Task.Delay(1000).Wait();
+Console.WriteLine("The commands are: kroblox(kills roblox) kstudio(kills roblox studio)");
+Console.WriteLine("This console window does close when you exeucte!");
+var _string = Console.ReadLine();
+if (_string == "kroblox")
+{
+    foreach (Process cls in Process.GetProcesses())
+    {
+        if (cls.ProcessName.Equals("RobloxPlayerBeta"))
+        {
+            Console.WriteLine("Found ROBLOX!");
+            cls.Kill();
+            Task.Delay(1000).Wait();
+            Console.WriteLine("ROBLOX has been killed!");
+        }
+    }
+}
+if (_string == "kstudio")
+{
+    foreach (Process cls in Process.GetProcesses())
+    {
+        if (cls.ProcessName.Equals("RobloxStudioBeta"))
+        {
+            Console.WriteLine("Found ROBLOX Studio!");
+            cls.Kill();
+            Task.Delay(1000).Wait();
+            Console.WriteLine("ROBLOX Studio has been killed!");
+        }
+    }
+}
